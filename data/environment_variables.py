ec76d2c79cc5f10431079eda139d6d8ba7cb0cdf
=Kalyan <putrevu.kalyan@gmail.com>
2025-05-09 15:34:13 -0400

    changes with .env

---
+++ data/environment_variables.py
@@ -0,0 +1,6 @@
+import os
+
+from dotenv import find_dotenv, load_dotenv
+
+# find .env file automatically by walking up directories until its found
+dotenv_path = find_dotenv()
---
+++ src/test_dataservice.py
@@ -0,0 +1,7 @@
+from services.dataservice import DataService
+
+data_service = DataService()
+
+data_service.run()
+
+data_service.data
